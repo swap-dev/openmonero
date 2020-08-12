ae5e25a4281cd2608b57e8c3750861046e116d9d
reeyon <reeyonlim@gmail.com>
2020-08-12 17:04:22 +0800

    frontend update - remote obsolete payment id tab - replace "light web wallet" text with api + blockheight - update footer - update WASM again

--- html/js/controllers/version.js
+++ html/js/controllers/version.js
@@ -16,13 +16,10 @@ thinwalletCtrls.controller('VersionCtrl', function ($scope, $http, ApiCalls) {
             var api_major = response.data.api >> 16;
             var api_minor = response.data.api & 0xffff;
 
-            $scope.version = "Open Monero version (api): "
-                + git_branch_name + "-" + last_git_commit_date + "-" + last_git_commit_hash
-                + " (" + api_major + "." + api_minor + ")"
-                + "  | Monero version: " + monero_version_full
-                + "  | Blockchain height: " + response.data.blockchain_height
+            $scope.version = "swap version: " + monero_version_full
+                + " |   Height: " + response.data.blockchain_height
 
         }, function(response) {
-            $scope.version = "Error: Can't connect to the backend! Maybe it is down."
+            $scope.version = "⛔ Error: Can't connect to the backend!"
         });
 });
